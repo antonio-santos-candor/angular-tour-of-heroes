9abc00236c24584c845cde50cfcc40ef72228d7c
António Santos <toi.santos@gmail.com>
2019-10-15 12:38:33 +0100

    Fixed server.js for Heroku

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const path = require('path');
 const app = express();
 const compression = require('compression');
 
-const locale = process.env.LOCALE;
-
 app.use(compression());
 
 // Serve only the static files from the dist directory
@@ -27,7 +25,7 @@ if (env === 'production') {
 
 
 
-app.use(express.static(__dirname + '/dist/' + locale));
+app.use(express.static(__dirname + '/dist'));
 
 // app.get('*', function (req, res, next) {
 //   console.log("It's a get *");
@@ -37,7 +35,7 @@ app.use(express.static(__dirname + '/dist/' + locale));
 // })
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/' + locale + '/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
